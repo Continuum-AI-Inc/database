9d13bf2a01776ec907677932bfeb2f2957b109a4
Moritz Utcke <moritz.utcke@gmx.de>
2023-11-17 14:48:03 +0545

    Fix postinstall and type errors

--- src/index.d.ts
+++ src/index.d.ts
@@ -2,7 +2,7 @@ import { PrismaClient } from "@prisma/client";
 
 export function createContext(ctx: any): Promise<Context>;
 
-export * from ".prisma/client/index.d";
+export * from "@prisma/client";
 
 export interface Context {
   prisma: PrismaClient;
